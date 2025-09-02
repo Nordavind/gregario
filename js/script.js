12286f1afc91046a91d8633f2d432f04e675dfe2
Cristobal Mauras <cristobal.mauras@gmail.com>
2025-09-01 21:33:13 -0400

    header y post header v1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  //navbar
+  const navbar = document.getElementById("navDeploy");
+  const navLink = document.getElementById('navLink');
+  const navList = navbar.querySelector('ul');
+
+  const toggleNavBar = (navbarElement, navLink) => {
+    navLink.classList.toggle("active");
+    navbarElement.classList.toggle("open");
+  };
+
+  navLink.addEventListener('click', () => toggleNavBar(navbar, navLink));
+
+  navList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A' || event.target.tagName === 'BUTTON') {
+      toggleNavBar(navbar, navLink);
+    }
+  });
+});
